56fc4a5385a8c7ed28e4c46b3184f0087b6b4fb1
januscx <januscx@gmail.com>
2023-10-16 01:09:08 +0200

    First test

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os.path import join, dirname
 
 setup(
     name='RESTAPIgit',
     version='1.0',
-    description='Тестовый проект ',
-    author='januscx',
-    author_email='author@gmail.com',
-    packages=['RESTAPIgit'],  # same as name
-#    install_requires=['numpy', 'pandas'],  # external packages as dependencies
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
