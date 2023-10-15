b370f0e2729066ecf4c7cfa91c1ea4dad6d35ff7
januscx <januscx@gmail.com>
2023-10-16 01:01:57 +0200

    First test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='RESTAPIgit',
+    version='1.0',
+    description='Тестовый проект ',
+    author='januscx',
+    author_email='author@gmail.com',
+    packages=['RESTAPIgit'],  # same as name
+#    install_requires=['numpy', 'pandas'],  # external packages as dependencies
+)
